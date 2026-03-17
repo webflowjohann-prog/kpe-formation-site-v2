7da51d4698cba9a8e2caffc7bcb3e128f90a67f7
IKONIK <webflow.johann@gmail.com>
2026-03-17 11:36:42 +0100

    Fix iframe rendering for active lesson video

--- src/components/EspaceEleve.jsx
+++ src/components/EspaceEleve.jsx
@@ -216,7 +216,7 @@ function ModuleView({ module, lessons, progress, session, onBack }) {
             <div>
               <div style={styles.videoPlaceholder}>
                 {activeLesson.video_url ? (
-                  <div dangerouslySetInnerHTML={{
+                  <div style={{ width: '100%', height: '100%' }} dangerouslySetInnerHTML={{
                     __html: `<iframe src="${activeLesson.video_url}" allow="autoplay; fullscreen; encrypted-media; picture-in-picture" allowfullscreen style="width:100%;height:100%;border:none;" />`
                   }} />
                 ) : (
